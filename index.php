688b8c07f4f3ca57b83596611cd50d5e977c6493
philipbraatz <philopbritz@gmail.com>
2018-01-30 13:45:44 -0600

    added picture

---
+++ index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"">
+    <title>Phil's Homepage></title>
+    <link rel="stylesheet" type="text/css" href="css/base.css">
+</head>
+<body>
+<header><h1>Phil's Website</h1></header>
+    <nav>
+        <ul>
+            <li><a href="/">Homepage</li>
+            <li><a href="#">Loop</li>
+            <li><a href="#">Countdown</li>
+        </ul>
+    </nav>
+    <main>
+        <img src="images/Philip_Braatz.jpg" alt="Philip Braatz"/>
+        <p>Info goes here</p>
+    </main>
+    <footer>&copy; 20xx PhilipBraatz.com</footer>
+</body>
+</html>
